14081cc0d21f1c8db54fab8852a6ce96be067544
krnand <1india.kng@gmail.com>
2023-11-22 21:32:41 +0530

    @Qualifier

--- src/main/java/org/springcore/autowire/annotation/Employee.java
+++ src/main/java/org/springcore/autowire/annotation/Employee.java
@@ -1,15 +1,17 @@
 package org.springcore.autowire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
     // 3way: 1. Property, 2. Setter, 3. Constructor
-    // @Autowired
+    @Autowired
+    @Qualifier("temp2") //Qualifier can't be added to constructor
     private Address address;
 
     public Employee() {
     }
-    @Autowired
+//    @Autowired
     public Employee(Address address) {
         this.address = address;
     }
